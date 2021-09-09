4b6618529510df2d6df763bd804108e2e076942e
IZerab <beltramelorenzo1999@gmail.com>
2021-09-09 23:04:25 +0200

    Project finished?
    I added the MLP regressor. This should be quite definitive

--- ML_functions.py
+++ ML_functions.py
@@ -14,6 +14,7 @@ from sklearn.linear_model import Ridge
 from sklearn.preprocessing import OneHotEncoder, StandardScaler, RobustScaler
 from sklearn.metrics import matthews_corrcoef, r2_score, accuracy_score, confusion_matrix
 from sklearn.metrics import plot_roc_curve, plot_precision_recall_curve, plot_confusion_matrix
+from sklearn.neural_network import MLPRegressor
 
 from sklearn.model_selection import train_test_split
 from sklearn.compose import make_column_transformer
@@ -22,7 +23,7 @@ from sklearn.compose import make_column_transformer
 # custom lib
 import make_dataset as m_d
 
-
+random_seed = 3413428
 ##MAGARI queste funzioni si faceva meglio a farle che ricevono solo l'insieme di test, o magari gli insiemi pre-splittati
     #Sarebbe stata più coerente la comparazione tra modelli
 
@@ -106,72 +107,33 @@ def Random_Forest_Regressor_CV(X,y, num_features):
     return grid_pipeline
 
 
-########### CLASSIFICAZIONE CIRCOSCRIZIONI ##############
-def circoscrizione_attiva(link):
+# Random forest
+def Neural_Regressor(X, y, num_features):
     """
-    Funzione che prende il dataframe raffinato dei tweets e determina per ogni giornata
-    la circoscrizione con più tweets
-    Ritorna una lista lunga 61 con i nomi delle circoscrizioni con più tweets per quel giorno
-    """
-    tw=pd.read_csv(link)
-
-    tw.dropna(subset=["circoscrizione"], inplace=True)
-    tw.reset_index(inplace=True)
-
-    temp=tw.groupby(["month", "day","circoscrizione"]).size()
-    maxi=temp.groupby(["month", "day"]).idxmax()
-
-    output=[i[2] for i in maxi]
-    return output
-
-
-
-def Random_Forest_Classifier_Circoscrizione(X, y, num_features, cat_features):
-    """
-    Funzione che crea, fitta, testa e ritorna una pipeline con il RFC regressor,
-    (questa volta in riferimento al problema della classificazione di circoscrizioni)
+    Funzione che crea, fitta, testa e ritorna una pipeline con il RF regressor,
+    (in riferimento al problema della regressione al numero di tweets)
     con alcune caratterstiche autoevidenti da codice
+    Non ha senso fare la CV vedi paper
     Input: X sono i dati, y i targets
-        num e cat features rappresentano features numeriche e categoriche (come lista di stringhe)
-    Printa l'accuracy del test
+        num features rappresenta features numeriche(come lista di stringhe)
+    Printa il risultato dell'r2 score
+
     """
-    X=X[num_features+cat_features]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
+    X = X[num_features]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-    transf=make_column_transformer(
-            (StandardScaler(), num_features),
-            (OneHotEncoder(handle_unknown="ignore"), cat_features),
-            remainder='drop')
-
-    pipe_RFC = Pipeline([
-        #('encoder', OneHotEncoder(sparse=False, handle_unknown='ignore')),
+    transf = make_column_transformer(
+        (StandardScaler(), num_features),
+        remainder='drop')
+    pipe_NN = Pipeline([
         ('transformer', transf),
-        ('Regressor', RandomForestClassifier(bootstrap=False))
+        ('Regressor', MLPRegressor(max_iter=600, random_state=random_seed))
     ])
 
-    # Provo con una grid search CV
-    CV_parameters = {'Regressor__n_estimators': [50, 100, 200, 500],  # Valori superiori rallentano l'algoritmo
-                     'Regressor__max_depth': [5, 10, 20, 50, 70, 100],  # Rasoio di Occam per evitare overfitting
-                     'Regressor__min_samples_leaf': [1, 2, 4],  # Sempre rasoio di Occam
-                     'Regressor__min_samples_split': [2, 5, 10, 15, 20],
-                     }
-    # Parametri di Tuning del nostro RFR
-    RFC_CV = GridSearchCV(estimator=pipe_RFC,
-                          param_grid=CV_parameters,
-                          n_jobs=-1,
-                          cv=2)
-    RFC_CV.fit(X_train, y_train)
-    y_RFC_pred = RFC_CV.predict(X_test)
-    print("Random forest classifier accuracy score:", accuracy_score(y_test, y_RFC_pred))
-    
-    #Gonna leave these here, more convenient than return the test values as in "return RFC_CV, (X_test, y_test)"
-    plot_confusion_matrix(RFC_CV, X_test, y_test)
-    plot_precision_recall_curve(RFC_CV, X_test, y_test)
-    plot_roc_curve(RFC_CV, X_test, y_test)
+    # Vale la pena fare un tentativo prima della CV che da un'accuracy 0
+    pipe_NN.fit(X_train, y_train)
+    y_NN_pred = pipe_NN.predict(X_test)
 
+    print("Neural Network r2_score = ", r2_score(y_test, y_NN_pred))
 
-    return RFC_CV
-
-
-    
-    
+    return pipe_NN
